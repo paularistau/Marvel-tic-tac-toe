90ac4883f2e59f344924fe8e6900ff63a213d0ef
Paula Ristau <paularistauf@gmail.com>
2020-09-01 03:36:17 -0300

    set selectcharacter in initial step

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
-
-
+import { CharactersApiService} from './characters/character/shared/characters-api.service';
+import {Characters} from './classes/Characters'
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'tic-tac-toe';
+  startNewGame: Boolean = false
 
-  
+  constructor(private _CharactersApiService: CharactersApiService){
+  }
+  lstCharacters: Characters[];
+
+  ngOnInit(){
+    this._CharactersApiService.getAllCharacters()
+    .subscribe(
+      data =>
+      {
+        this.lstCharacters = data;
+      }
+    )
+  }
+
+  startGame(){
+    this.startNewGame = true
+    console.log('startNewGame game', this.startNewGame)
+  }
 }
